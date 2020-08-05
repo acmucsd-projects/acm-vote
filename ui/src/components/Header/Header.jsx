1e90a077418f82f7853636b337a281f47061bd22
YiyueMaggieMao <myykitty711@gmail.com>
2020-08-05 23:02:18 +0800

    Made header so that the profile photo can be passed down from outside

--- ui/src/components/Header/Header.jsx
+++ ui/src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import profilePic from '../../img/profile.png';
 import './Header.css';
 
 /* Profile photo link  be passed in from the Page component */
-const Header = (imgLink) => {
+const Header = (props) => {
     return(
         <div className="header">
-            <img className="profile-pic" src={profilePic} alt="Profile Pic"/>
+            <img className="profile-pic" src={require(`../../img/${props.imgLink}`)} alt="Profile Pic"/>
         </div>
     );
 }
