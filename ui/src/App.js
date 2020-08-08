3bc935ebfd3ffdf8e6d5e009402de750509b8f05
YiyueMaggieMao <myykitty711@gmail.com>
2020-08-08 23:54:05 +0800

    Changed App.js to show the page layout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from './components/Header/Header';
+import PageLayout from './components/PageLayout/PageLayout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header imgLink="profile.png" />
+      <PageLayout />
     </div>
   );
 }
