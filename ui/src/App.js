b0d0597e6048a29462f4fa33949dec0875c4b69f
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2020-08-14 13:32:47 +0530

    login + home page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from "./Home";
+import Login from "./Login";
 import './App.css';
+import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header imgLink="profile.png" />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login}/>
     </div>
   );
 }
