5d6a4cded93e075cfda8249e9210ab6dc2647049
michl1001 <mlshaojr@gmail.com>
2020-08-20 14:17:48 -0400

    PSQL fixes

--- api/initialize.py
+++ api/initialize.py
@@ -1,11 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
-from models.py import {votes, users}
-
-engine = create_engine(DATABASE_URI)
-
-Base = declarative_base()
+from models import Base, engine
 
 Base.metadata.create_all(engine)
--- api/models.py
+++ api/models.py
@@ -1,15 +1,38 @@
-from initialize import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+from sqlalchemy import Column, Integer, String, Boolean, DATETIME
+from sqlalchemy.dialects.postgresql import JSON, ARRAY
+
+from config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+class election(Base):
+    __tablename__ = 'election'
+    id = Column(Integer, primary_key = True)
+    questions = Column(ARRAY(Integer))
+    hasVoted = Column(JSON) # ids of users who have voted in this election w/ timestamp
+    active = Column(Boolean)
+    creator = Column(Integer)
+    deadline = Column(DATETIME)
+
+class question(Base):
+    __tablename__ = 'question'
+    id = Column(Integer, primary_key = True)
+    question = Column(String)
+    votes = Column(JSON)
 
 
+class user(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    userName = Column(String)
+    uuid = Column(Integer)
+    canVote = Column(ARRAY(Integer)) # id numbers of whitelisted elections
+    boardMember = Column(Boolean)
+    email = Column(String)
 
-class votes(Base):
-   __tablename__ = 'votes'
-   id = Column(Integer, primary_key = True)
-   choice = Column(String)
-
-class users(Base):
-   __tablename__ = 'users'
-   id = Column(Integer, primary_key = True)
-   userName = Column(String)
-   hasVoted = Column(Boolean)
