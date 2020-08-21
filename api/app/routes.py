4b243309e843ea2a86fb13884ee86dcfaf45503b
michl1001 <mlshaojr@gmail.com>
2020-08-21 17:44:55 -0400

    Set up basic routes

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,6 +1,38 @@
-from app import app
+from . import app
 
 
-@app.route('/')
-def index():
+@app.route('/api/user/<uuid>', methods=['GET'])
+def getVoterInfo(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election', methods=['POST'])
+def createNewElection():
+    return "Hello, World!"
+
+@app.route('/api/election', methods=['GET'])
+def getAllElections():
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>', methods=['GET'])
+def getElections(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>', methods=['DELETE'])
+def deleteElection(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>', methods=['PATCH'])
+def editElection(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>/activate', methods=['PUT'])
+def activateElection(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>/vote', methods=['PUT'])
+def voteElection(uuid):
+    return "Hello, World!"
+
+@app.route('/api/election/<uuid>/result', methods=['PUT'])
+def getElectionResults(uuid):
     return "Hello, World!"
