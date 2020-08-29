754127fd17275bc776b5ebf24c13bd6e59e76bff
YiyueMaggieMao <myykitty711@gmail.com>
2020-08-29 13:01:31 +0800

    Redefined screen relationships

--- ui/src/components/PollBody/PollBody.jsx
+++ ui/src/components/PollBody/PollBody.jsx
@@ -6,8 +6,7 @@ const PollBody = () => {
     const [privacy, setPrivacy] = useState('private');
 
     /* Use an array to store page names to make the code more concise :0 */
-    const pageNames = ['basicInformation', 'setPrivacy-1', 'setPrivacy-2', 'setPrivacy-3', 
-                        'setAnswerOptions', 'confirmDetails', 'expiration'];
+    const pageNames = ['basicInformation', 'setPrivacy', 'confirmDetails', 'expiration'];
 
     /* Returns whether a section is currently visible */
     const getVisibility = (pageNumber) => {
@@ -17,16 +16,16 @@ const PollBody = () => {
     /* Returns the title of the current page */
     const getTitle = () => {
         if (currPage == 0) { return "Create Poll"; }
-        else if (currPage <= 3) { return "Set Privacy"; }
-        else if (currPage === 4) { return "Set Answer Options"; }
-        else if (currPage === 5) { return "Confirm Details"; }
-        else if (currPage === 6) { return "Expiration"; }
+        else if (currPage == 1) { return "Set Privacy"; }
+        else if (currPage === 2) { return "Set Answer Options"; }
+        else if (currPage === 3) { return "Confirm Details"; }
+        else if (currPage === 4) { return "Expiration"; }
         return "Invalid Page";
     }
 
     /* Top Button: Title */
     const getTopButtonText = () => {
-        if(currPage == 5){return "Finish";}
+        if(currPage == 3){return "Finish";}
         return "Continue";
     }
 
@@ -35,7 +34,7 @@ const PollBody = () => {
         // This is a special case because we will proceed to different pages depending on privacy settings
         if(currPage == 0){
             if(privacy === 'private'){setCurrPage(1);}
-            else{setCurrPage(4);}
+            else{setCurrPage(2);}
         }
         else{setCurrPage(currPage + 1);}
     }
@@ -47,7 +46,7 @@ const PollBody = () => {
 
     /* Top button: Determines which function to execute depending on the current page */
     const topButtonFunction = () => {
-        if(currPage == 4){createPoll();}
+        if(currPage == 3){createPoll();}
         else{continueToNextPage();}
     }
 
@@ -65,8 +64,8 @@ const PollBody = () => {
     /* Bottom button function: Goes back */
     const goBacktoLastPage = () => {
         // Special handling of Page 'Set Answer Options'
-        if(currPage == 4){
-            if(privacy === 'private') {setCurrPage(3);}
+        if(currPage == 2){
+            if(privacy === 'private') {setCurrPage(1);}
             else {setCurrPage(0);}
         }
         else{setCurrPage(currPage - 1);}
@@ -106,11 +105,13 @@ const PollBody = () => {
                     placeholder="What is the last day to vote?" />
                 <p>Do you want to create a Private or Public poll?</p>
                 <div className="radio-containers">
-                    <input type="radio" name="privacy" value="public" onClick={() => setPrivacy('public')} />
+                    <input type="radio" name="privacy" id="public" value="public" 
+                        checked={privacy==='public'} onClick={() => setPrivacy('public')} />
                     <label for="public">Public so all ACM members can view</label>
                 </div>
                 <div className="radio-containers">
-                    <input type="radio" name="privacy" value="private" checked onClick={() => setPrivacy('private')} />
+                    <input type="radio" name="privacy" id="private" value="private" 
+                        checked={privacy==='private'} onClick={() => setPrivacy('private')} />
                     <label for="private">Private so only ACM members I select can view</label><br></br>
                 </div>
             </div>
