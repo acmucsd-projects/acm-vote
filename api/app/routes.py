22ac51bd5b734a6e131fdbb330c492bf0a5a28d7
michl1001 <mlshaojr@gmail.com>
2020-09-04 01:10:42 -0400

    Addressed PR fixes

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,49 +1,69 @@
 from . import app
+from . import models
 
 @app.route('/api/user', methods=['GET'])
 def getAllVoters():
-    return "Hello, World!"
+    return json.dump(User.query.all())
 
-@app.route('/api/user/<uuid>', methods=['GET'])
+@app.route('/api/user/<int:uuid>', methods=['GET'])
 def getVoterInfo(uuid):
-    return "Hello, World!"
+    return json.dump(User.query.filter_by(id=uuid).first())
 
+'''
+{
+    name:"",
+    description:"",
+    questions:{
+        "insert_question_here":{
+            answers:["answer1","answer2","answer3"],
+            type:""
+        },
+        "insert_question_here":{
+            answers:["answer1","answer2","answer3"],
+            type:""
+        },
+    }
+    ,
+    users:[list here],
+    creator:[ID here],
+    deadline:[datetime here]
+}
+'''
 @app.route('/api/election', methods=['POST'])
 def createNewElection():
-    return "Hello, World!"
+    data = request.form.toJson()
+    for q in data['questions']:
+        quest = q
+    elect = election(name=request.form['name'], description=request.form['description'], )
 
 @app.route('/api/election', methods=['GET'])
 def getAllElections():
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>', methods=['GET'])
+@app.route('/api/election/<int:uuid>', methods=['GET'])
 def getElections(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>', methods=['DELETE'])
+@app.route('/api/election/<int:uuid>', methods=['DELETE'])
 def deleteElection(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>', methods=['PATCH'])
+@app.route('/api/election/<int:uuid>', methods=['PATCH'])
 def editElection(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>/activate', methods=['PUT'])
+@app.route('/api/election/<int:uuid>/activate', methods=['PUT'])
 def activateElection(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>/vote', methods=['POST'])
+@app.route('/api/election/<int:uuid>/vote', methods=['POST'])
 def voteElection(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>/results', methods=['GET'])
+@app.route('/api/election/<int:uuid>/results', methods=['GET'])
 def getElectionResults(uuid):
     return "Hello, World!"
 
-@app.route('/api/election/<uuid>/verify', methods=['GET'])
-def verifyElectionResults(uuid):
-    return "Hello, World!"
-
-@app.route('/api/election/<uuid>/audit', methods=['GET'])
+@app.route('/api/election/<int:uuid>/audit', methods=['GET'])
 def auditElectionResults(uuid):
     return "Hello, World!"
