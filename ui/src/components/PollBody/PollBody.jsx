c6fb5368f40b64e7af55731d89f059c814eddf73
YiyueMaggieMao <myykitty711@gmail.com>
2020-09-09 00:37:49 +0800

    Poll options show up correctly

--- ui/src/components/PollBody/PollBody.jsx
+++ ui/src/components/PollBody/PollBody.jsx
@@ -296,12 +296,17 @@ const PollBody = () => {
 
     /*------------------------------------------CONFIRM DETAILS PAGE---------------------------------------- */
     const confirmDetails = () => {
+        const listBody = options.map((option) => <li className="options-list-item">{option}</li>);
         return (
             <div className={getVisibility(3)}>
                 <p><span className="create-poll-bold">Title:{'\u00A0'}</span>{pollTitle}</p>
                 <p><span className="create-poll-bold">Description:{'\u00A0'}</span>{pollDescription}</p>
                 <p><span className="create-poll-bold">Poll Type:{'\u00A0'}</span>{pollType}</p>
                 <p><span className="create-poll-bold">Expiration:{'\u00A0'}</span>{pollExpiration}</p>
+                <p><span className="create-poll-bold">Poll Options:{'\u00A0'}</span></p>
+                <div className="options-list">
+                <ul className="options-list">{listBody}</ul>
+                </div>
             </div>
         )
     }
