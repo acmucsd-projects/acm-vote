bb6c339caf328edf2c1aa83a4abfb588a700aba6
YiyueMaggieMao <myykitty711@gmail.com>
2020-09-12 10:32:05 +0800

    Corrected links

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -3,14 +3,19 @@ import PageLayout from '../components/PageLayout/PageLayout';
 import './style.css';
 
 function Login() {
+
+    const redirectToACMRegister = () => {
+        window.location.href='https://members.acmucsd.com/register'
+    }
+
     return (
         <PageLayout>
             <h1>acm vote</h1>
             <input type="text" placeholder="Email"></input>
             <input type="password" placeholder="Password"></input>
-            <a href="#" id="forgot-password">Forgot Password?</a>
-            <input type="button" value="Sign In"/>
-            <input type="button" value="Create New Account" onClick="window.location = 'https://members.acmucsd.com/login';"/>
+            <a href="https://members.acmucsd.com/forgot-password" id="forgot-password">Forgot Password?</a>
+            <button className="login-button">Sign in</button>
+            <button className="login-button" onClick={redirectToACMRegister}>Create New Account</button>
         </PageLayout>
     );
 }
