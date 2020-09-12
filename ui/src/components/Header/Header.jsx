c48271a8198ed44831c44d39020e93755fa9099e
tylerred19 <58914843+tylerred19@users.noreply.github.com>
2020-09-12 08:27:18 -0700

    edit header

--- ui/src/components/Header/Header.jsx
+++ ui/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
-import sampleProfilePic from '../../img/profile.png';
+import sampleProfilePic from '../../img/hamburger-menu.png';
+import sampleProfilePic2 from '../../img/acm-logo-final.png';
 
 /* Profile photo link  be passed in from the Page component */
 const Header = (props) => {
     return(
         <div className="header">
+            <img className="acm-logo" src={sampleProfilePic2} alt="Profile Pic"/>
             <img className="profile-pic" src={sampleProfilePic} alt="Profile Pic"/>
         </div>
     );
