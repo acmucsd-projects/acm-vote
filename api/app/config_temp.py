61c8ec816ad13712d37dc2b3d35fa870dd22db97
michl1001 <mlshaojr@gmail.com>
2020-09-15 21:08:05 -0400

    Modified config file to accomodate new code

--- api/app/config_temp.py
+++ api/app/config_temp.py
@@ -5,3 +5,20 @@ TEST_SERVER = True  # If database is being seeded, decides whether to create
                     # example elections and questions
 
 MEMBERSHIP_API = '' # Host Port for the membership portal 
+
+headers = {
+  'Content-Type': 'application/json'
+}
+payload = {
+    'email':'', # Membership Portal account email
+    'password': '' # Membership Portal account password
+}
+
+# Membership Portal Login
+token_response = requests.post(MEMBERSHIP_API + 'api/v1/auth/login', data = json.dumps(payload), headers=headers)
+token_json =  token_response.json()
+
+TOKEN = { # Token to access Membership Portal requests
+    'Content-Type': 'application/json',
+    "Authorization": "Bearer " + token_json['token']
+}
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,14 +1,19 @@
 from . import app
 from . import models
+from config import TOKEN, MEMBERSHIP_API
 import json
+import requests
 
-@app.route('/api/user', methods=['GET'])
-def getAllVoters():
-    return json.dump(User.query.all())
+@app.route('/api/user/login', methods=['POST'])
+def getVoterlogin():
 
-@app.route('/api/user/<int:uuid>', methods=['GET'])
+    r = request.get(MEMBERSHIP_API + "", headers=TOKEN)
+    return json.dump(r.json())
+
+@app.route('/api/user/<string:uuid>', methods=['GET'])
 def getVoterInfo(uuid):
-    return json.dump(User.query.filter_by(id=uuid).first())
+    r = request.get(MEMBERSHIP_API + "api/v1/user/", headers=TOKEN)
+    return json.dump(r.json())
 
 '''
 {
@@ -32,27 +37,47 @@ def getVoterInfo(uuid):
 '''
 @app.route('/api/election', methods=['POST'])
 def createNewElection():
-   return "Hello, World!"
+    data = request.form.toJson()
+    for q in data['questions']:
+      quest = q
+
+    elect = Election(name=request.form['name'], description=request.form['description'], )
 
 @app.route('/api/election', methods=['GET'])
-def getAllElections():
-    return "Hello, World!"
+def getElections():
+    return json.dumps(Election.query.all())
 
 @app.route('/api/election/<int:uuid>', methods=['GET'])
-def getElections(uuid):
-    return "Hello, World!"
+def getElection(uuid):
+    return json.dumps(Elections.query.filter_by(id=uuid))
 
 @app.route('/api/election/<int:uuid>', methods=['DELETE'])
 def deleteElection(uuid):
-    return "Hello, World!"
+    election = Elections.query.filter_by(id=uuid)
+    if(election['active'] == true):
+        return "ERROR - Cannot delete active election"
+    
+    else:
+        db.session.delete(election)
+        db.session.commit()
 
 @app.route('/api/election/<int:uuid>', methods=['PATCH'])
 def editElection(uuid):
+    election = Elections.query.filter_by(id=uuid)
+    if(election['active'] == true):
+        return "ERROR - Cannot edit active election"
+    
+    else:
+        
     return "Hello, World!"
 
 @app.route('/api/election/<int:uuid>/activate', methods=['PUT'])
 def activateElection(uuid):
-    return "Hello, World!"
+    election = Elections.query.filter_by(id=uuid)
+    if(election['active'] == true):
+        return "ERROR - Already active election"
+    
+    else:
 
 @app.route('/api/election/<int:uuid>/vote', methods=['POST'])
 def voteElection(uuid):
