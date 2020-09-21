246dacaf818b1380f40d7e249e03c1900ba13904
Yiyue (Maggie) Mao <46667607+YiyueMaggieMao@users.noreply.github.com>
2020-09-21 15:46:02 +0800

    Vote page (#27)
    * Added basic vote page layout with hardcoded data
    
    * Added basic layout for Vote main page
    
    * Fixed input styles
    
    * Fleshed out button functionalities
    
    * Background successfully blurs on submit!
    
    * Rendered popup window that can redirect back home
    
    * Added API call to get an election
    
    * Removed console log
    
    Co-authored-by: Matei-Alexandru Gardus <matei@gard.us>
    
    * Update option mapping functionalities
    
    Thanks Matei :D
    
    Co-authored-by: Matei-Alexandru Gardus <matei@gard.us>
    
    * Used ant design notifications
    
    Co-authored-by: Matei-Alexandru Gardus <matei@gard.us>

---
+++ ui/src/pages/Vote.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import PageLayout from '../components/PageLayout/PageLayout';
+import PollVoteHome from '../components/PollVoteHome/PollVoteHome';
+import PollVoteMain from '../components/PollVoteMain/PollVoteMain';
+import hardCodedPoll from '../data/HardCodedPoll.json';
+// import API from '../API';
+import './style.css';
+
+const Vote = () => {
+    const {uuid} = useParams();
+    const [currPage, setCurrPage] = useState(0);
+    const [election, setElection] = useState({});
+    useEffect(() => {
+        /*API.getPoll(uuid)
+        .then((response) => {
+            setElection(response);
+        })*/
+        setElection(hardCodedPoll);
+    })
+
+    // I probably can't use the const {name, description} = election here
+    // Because JavaScript does have restrictions with the use of "name"
+
+    /* Render the corresponding page depending on the current state */
+    const pageContent = currPage === 0 ? 
+            <PollVoteHome pollTitle={election.name} pollId={election.id} 
+            pollDescription={election.description} deadline={election.deadline} 
+            setCurrPage={setCurrPage} /> : 
+            <PollVoteMain pollTitle={election.name} pollDescription={election.description}
+            pollOptions={election.questions} deadline={election.deadline}
+            setCurrPage={setCurrPage}/>;
+
+    return (
+        <PageLayout>
+            {pageContent}
+        </PageLayout>
+    )
+}
+
+export default Vote;
