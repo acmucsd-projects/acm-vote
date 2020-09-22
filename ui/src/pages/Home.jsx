7f59a65e114199a8c09c25d82dc135f259dcfa5d
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2020-09-22 06:19:25 +0530

    My Polls + Prettify

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function Home() {
+
+    const create = () => {
+        window.location.href='/create';
+    }
+
+    const myPolls = () => {
+        window.location.href='/my-polls';
+    }
+
     return (
         <div>
             <h1>Home</h1>
-            <input type="button" value="Create New Poll"/>
-            <input type="button" value="View My Polls"/>
+            <input type="button" value="Create New Poll" onClick={create} />
+            <input type="button" value="View My Polls" onClick={myPolls} />
             <input type="button" value="View All Polls"/>
         </div>
     );
