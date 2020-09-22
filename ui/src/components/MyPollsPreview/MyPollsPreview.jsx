5a37b05f8e3c978a6f01f187306d6a5cc8f2ec42
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2020-09-22 07:29:58 +0530

    Revision Edits
    Changed from class component to functional. CSS properties defined for draft and active polls to help with middleware integration.

--- ui/src/components/MyPollsPreview/MyPollsPreview.jsx
+++ ui/src/components/MyPollsPreview/MyPollsPreview.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import './MyPollsPreview.css';
 
-class MyPollsPreview extends React.Component {
-    render() {
-        const isFinished = (this.props.pollId != -1);
+const MyPollsPreview = (props) => {
+    
+    // TODO change when middleware is added
+    const isFinished = props.pollId != -1;
 
-        // default unfinished draft
-        var backgroundColor = '#62B0FF';
-        var pollId = "Unfinished draft" 
-
-        if (isFinished) {
-            backgroundColor = '#816DFF';
-            pollId = "Poll ID: " + this.props.pollId;
-        }
-        return(
-            <a href="#" id="previewBox" style={{ 'background-color': backgroundColor }} >
-                <div id="pollName">{this.props.pollName}</div>
-                <div id="pollId">{ pollId }</div>
+    if (isFinished) {
+        return (
+            <a href="#" id="previewBox" className={ 'background-finished' }>
+                <div id="pollName">{ props.pollName }</div>
+                <div id="pollId">Poll ID: { props.pollId }</div>
             </a>
-        );
+        )
+    }
+    else {
+        return (
+            <a href="#" id="previewBox" className={ 'background-draft' }>
+                <div id="pollName">{ props.pollName }</div>
+                <div id="pollId">Unfinished draft</div>
+            </a>
+        )
     }
 }
 
