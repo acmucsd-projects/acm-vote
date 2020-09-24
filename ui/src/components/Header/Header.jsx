1c7088ecefcb130d48391ec0835ceb424862288b
tylerred19 <58914843+tylerred19@users.noreply.github.com>
2020-09-23 20:59:33 -0700

    Prettify Header (#25)
    Co-authored-by: Matei-Alexandru Gardus <matei@gard.us>

--- ui/src/components/Header/Header.jsx
+++ ui/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
-import sampleProfilePic from '../../img/profile.png';
+import hamburger from '../../img/hamburger-menu.png';
+import acmVoteLogo from '../../img/acm-logo-final.png';
 
 /* Profile photo link  be passed in from the Page component */
 const Header = (props) => {
     return(
         <div className="header">
-            <img className="profile-pic" src={sampleProfilePic} alt="Profile Pic"/>
+            <img className="acm-logo" src={acmVoteLogo} alt="Acm Logo"/>
+            <img className="hamburger" src={hamburger} alt="Profile Pic"/>
         </div>
     );
 }
