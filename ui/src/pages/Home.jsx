ba5ddbeb96cb64ea105ef3336b3e055ee9d2c999
tylerred19 <58914843+tylerred19@users.noreply.github.com>
2020-09-24 10:20:50 -0700

    update graphic margin top

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -6,7 +6,7 @@ console.log(graphic);
 function Home() {
     return (
         <div>
-            <img src={graphic} alt="ACM Vote Ballot" />
+            <img src={graphic} class="acm-vote-graphic" alt="ACM Vote Ballot"/>
             <input type="button" class="vote-button" value="Vote"/>
             <input type="button" class="create-button" value="Create Poll"/>
             <input type="button" class="my-button" value="View My Polls"/>
