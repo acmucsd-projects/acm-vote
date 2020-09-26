5aa2b8784e299a4c34576951a1f3e4c58dec461b
Matei-Alexandru Gardus <matei@gard.us>
2020-09-25 18:28:03 -0700

    Add Login functionality to all pages
    Login now required to access any page from UI except the login page.
    Token is saved in local storage and redirection is guaranteed by href.
    
    Things that can be revised here:
    - I'm sure React is not a fan of `window.location.href`. Maybe a React
      Router would be better here.
    - Pretty much every function could be moved in different folders to
      refactor looks and stuff.
    - Linting *must* be set everywhere in the API and UI sooner rather than
      later. This will be fixed as soon as possible, most likely after
      the showcase.

--- ui/src/API.js
+++ ui/src/API.js
@@ -1,32 +1,43 @@
-import axios from 'axios';
-
-// I can't seem to find the server URL in the backend just yet:((
-const serverURL = "";
+import axios from "axios";
+const serverURL = "https://vote.acmucsd.com";
 
 export default {
-    createPoll: function(payload) {
-        const options = payload.options.filter(option => {
-            return option.optionName && option.description;
-        })
+  createPoll: function (payload) {
+    const options = payload.options.filter((option) => {
+      return option.optionName && option.description;
+    });
 
-        const config = {
-            method: 'post',
-            url: `${serverURL}/api/election`,
-            // Help: How do I figure out the ID?
-            id: 10086,
-            name: payload.pollTitle,
-            description: payload.pollDescription,
-            questions: options, // I assume this is options?
-            active: true,
-            creator: 1234567, // Probably need login to be finished for this?
-            deadline: payload.deadline
-            // What about poll type and voters :0
-        }
+    const config = {
+      method: "post",
+      url: `${serverURL}/api/election`,
+      // Help: How do I figure out the ID?
+      id: 10086,
+      name: payload.pollTitle,
+      description: payload.pollDescription,
+      questions: options, // I assume this is options?
+      active: true,
+      creator: 1234567, // Probably need login to be finished for this?
+      deadline: payload.deadline,
+      // What about poll type and voters :0
+    };
 
-        return axios(config);
-    },
+    return axios(config);
+  },
 
-    getPoll: function(uuid) {
-       return axios.get(`${serverURL}/api/election/${uuid}`); 
-    }
-}
+  getPoll: function (uuid) {
+    return axios.get(`${serverURL}/api/election/${uuid}`);
+  },
+
+  loginUser: (email, password) => {
+    const config = {
+      method: "post",
+      url: `${serverURL}/api/user/login`,
+
+      data: {
+        email: email,
+        password: password,
+      },
+    };
+    return axios(config);
+  },
+};
--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import PageLayout from '../components/PageLayout/PageLayout';
-import acmVoteLogo from './../img/acm-logo-final.png';
-import './style.css';
+import React, {useState} from "react";
+import PageLayout from "../components/PageLayout/PageLayout";
+import acmVoteLogo from "./../img/acm-logo-final.png";
+import API from "../API";
+import {notification} from "antd";
+import storage, {history} from "../storage";
+import "./style.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const redirectToACMRegister = () => {
+    window.location.href = "https://members.acmucsd.com/register";
+  };
 
-    const redirectToACMRegister = () => {
-        window.location.href='https://members.acmucsd.com/register'
+  const handleChange = (e) => {
+    switch (e.target.id) {
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
     }
+  };
 
-    return (
-        <PageLayout>
-            <img className="login-logo" src={acmVoteLogo} alt="Acm Logo"/>
-            <input type="text" placeholder="Email"></input>
-            <input type="password" placeholder="Password"></input>
-            <a href="https://members.acmucsd.com/forgot-password" id="forgot-password">Forgot Password?</a>
-            <button className="login-button sign-in-button">Sign in</button>
-            <button className="login-button create-account-button" onClick={redirectToACMRegister}>Create New Account</button>
-        </PageLayout>
-    );
+  const handleSubmit = (e) => {
+    API.loginUser(email, password)
+      .then((loginResponse) => {
+        if (loginResponse.data.error !== null) {
+          notification.open({
+            key: "login-500",
+            message: "Error logging in",
+            description: `${loginResponse.data.error}`,
+          });
+          return;
+        }
+        storage.set("token", loginResponse.data.token);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        notification.open({
+          key: "login-500",
+          message: "Error logging in",
+          description: `${error.message}`,
+        });
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <PageLayout>
+      <img className="login-logo" src={acmVoteLogo} alt="Acm Logo" />
+      <form onSubmit={handleSubmit}>
+        <input
+          id="email"
+          value={email}
+          onChange={handleChange}
+          type="text"
+          placeholder="Email"
+        ></input>
+        <input
+          id="password"
+          value={password}
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+        ></input>
+        <a
+          href="https://members.acmucsd.com/forgot-password"
+          id="forgot-password"
+        >
+          Forgot Password?
+        </a>
+        <button type="submit" className="login-button sign-in-button">
+          Sign in
+        </button>
+        <button
+          className="login-button create-account-button"
+          onClick={redirectToACMRegister}
+        >
+          Create New Account
+        </button>
+      </form>
+    </PageLayout>
+  );
 }
 
-export default Login
+export default Login;
